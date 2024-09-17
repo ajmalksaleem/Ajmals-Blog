fc1083fbc0d42d662379bd248361959258d297c8
ajmal k saleem <ajmalksaleem@gmail.com>
2024-09-17 15:05:50 +0530

    created the header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import About from './pages/About'
+import Header from './components/Header'
 const App = () => {
   return (
-    <h1 className='text-3xl text-blue-500'>hello</h1>
+    <BrowserRouter>
+    <Header/>
+<Routes>
+<Route path="/about" element={<About/>}/>
+</Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>about nnnnnnnnnnnnnnnnnnnnn
+      helo
+    </div>
+  )
+}
+
+export default About
